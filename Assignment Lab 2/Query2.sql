fc5f0d7242b58d9efdbe655ae917027dceef78c0
juicy.lemon_ <macjessie.bartolome@gmail.com>
2025-02-15 17:33:25 +0800

    Update Query2.sql

--- Assignment Lab 2/Query2.sql
+++ Assignment Lab 2/Query2.sql
@@ -11,10 +11,10 @@ FROM
     cust_vendor_dim cvd,
     trans_type_dim ttd
 WHERE 
-    inf.DateKey = dd.DateKey  -- Connect inventory_fact to date_dim
-    AND inf.CustVendorKey = cvd.CustVendorKey  -- Connect inventory_fact to cust_vendor_dim
-    AND inf.TransTypeKey = ttd.TransTypeKey  -- Connect inventory_fact to trans_type_dim
-    AND ttd.TransTypeKey = 5  -- Filter for shipments (transaction type 5)
-    AND dd.CalYear IN (2011, 2012)  -- Filter for calendar years 2011 and 2012
+    inf.DateKey = dd.DateKey
+    AND inf.CustVendorKey = cvd.CustVendorKey
+    AND inf.TransTypeKey = ttd.TransTypeKey
+    AND ttd.TransTypeKey = 5
+    AND dd.CalYear IN (2011, 2012)
 GROUP BY 
     ROLLUP (dd.CalYear, dd.CalQuarter, cvd.Zip, cvd.Name);
